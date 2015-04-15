54ee0d1d6d4e28e8edded57b7c195baa78d20a56
Emil Karlsson <ek222mw@student.lnu.se>
2015-04-15 12:03:29 +0200

    Uppdaterat templates teman för olika sidor

--- wordpress/wp-content/themes/twentythirteen/template_achievements.php
+++ wordpress/wp-content/themes/twentythirteen/template_achievements.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: MyOwnCode
+Template Name: Achievements
 */
 ?>
 <?php
@@ -55,9 +55,7 @@ get_header(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 					</div><!-- .entry-content -->
 
-					<footer class="entry-meta">
-						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
+					
 				</article><!-- #post -->
 
 				<!--<?php comments_template(); ?>-->
