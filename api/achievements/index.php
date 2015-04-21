7a333c73c921af068df2fb9c306b8c085c4acc9d
me222rs <me222rs@student.lnu.se>
2015-04-21 15:44:57 +0200

    Fixat get för achivement

--- api/achievements/index.php
+++ api/achievements/index.php
@@ -11,15 +11,22 @@
 	<input type="text" name="password" value="">
         <input type="submit" value="Skicka">
 </form>
-<h3>GET</h3>
+<h3>GET one achivement </h3>
 <form action="http://127.0.0.1/achievements/info.php" method="GET">
-    <br>Achievement
-	<input type="text" name="achievement" value="10 Kills">
+    <br>Achievement name
+	<input type="text" name="achievementName" value="10 Kills">
 	<br>Username
 	<input type="text" name="username" value="Admin">
-	<br>Achievement is done
-	<input type="number" name="achievementIsDone" value="1">
-	<br>Password
-	<input type="text" name="password" value="" disabled>
+
+        <input type="submit" value="Skicka">
+</form>
+
+<h3>GET all achievements on user</h3>
+<form action="http://127.0.0.1/achievements/info.php" method="GET">
+    <br>Achievement name
+	<input type="text" name="achievementName" value="10 Kills">
+	<br>Username
+	<input type="text" name="username" value="Admin">
+
         <input type="submit" value="Skicka">
 </form>
--- api/achievements/info.php
+++ api/achievements/info.php
@@ -7,32 +7,76 @@
 	$dbh = $conn->getConnection();
 	
 	$username = isset($_GET['username']) ? $_GET['username'] :  "";
-	$achievement = isset($_GET['achievement']) ? $_GET['achievement'] :  "";
-	$achievementIsDone = isset($_GET['achievementIsDone']) ? $_GET['achievementIsDone'] :  "";
+	$achievementName = isset($_GET['achievementName']) ? $_GET['achievementName'] :  "";
+	//$achievementIsDone = isset($_GET['achievementIsDone']) ? $_GET['achievementIsDone'] :  "";
 	
-	if(!empty($username) && !empty($achievement)){
+	if(!empty($username) && !empty($achievementName)){
 		
-		$sql = "SELECT * FROM wp_achievements WHERE username= ? AND achievement= ? AND achievementIsDone= ?";
-		$params = array($username, $achievement, $achievementIsDone);
+		$sql = "SELECT * FROM test_achievementisdone WHERE username= ? AND name= ?";
+		$params = array($username, $achievementName);
 		$query = $dbh -> prepare($sql);
 		$query -> execute($params);
 		$rows = $query -> fetchColumn();
 		
 		if($rows)
 		{
-			$sql = "SELECT * FROM wp_achievements WHERE username= ? AND achievement= ? AND achievementIsDone= ?";
-			$params = array($username, $achievement, $achievementIsDone);
+			$sql = "SELECT ta.name, taid.isDone, taid.username
+					FROM test_achievement AS ta
+					INNER JOIN test_achievementisdone AS taid 
+					ON ta.name = taid.name
+					WHERE taid.username = ?";
+			$params = array($username);
 			$query = $dbh -> prepare($sql);
 			$query -> execute($params);
 			$result = $query -> fetch();
-
-			$json = array("status" => 1, "achievement" => $result['achievement'], "achievementIsDone" => $result['achievementIsDone'], "username" => $result['username']);
+			
+			$json = array("status" => 1, "name" => $result['name'], "is done" => $result['isDone'], "username" => $result['username']);
 		}
 		else{
 			$json = array("status" => 0, "msg" => "Username and or achievement does not exist");
 		}
 		
-	}else{
+	}
+	elseif(!empty($username) && empty($achievementName)){
+		
+			$sql = "SELECT *
+					FROM test_achievementisdone
+					WHERE username = ?";
+			$params = array($username);
+			$query = $dbh -> prepare($sql);
+			$query -> execute($params);
+			$result = $query -> fetchAll();
+
+			
+		
+		$json_data=array();//create the array  
+		foreach($result as $rec)//foreach loop  
+		{  
+			$json_array['achievementIsDoneID']=$rec['achievementIsDoneID'];   
+			$json_array['username']=$rec['username'];  
+			$json_array['isDone']=$rec['isDone'];
+			$json_array['name']=$rec['name']; 			
+		//here pushing the values in to an array  
+			array_push($json_data,$json_array);  
+		  
+		}  
+		$json = $json_data;
+		
+			//$json1 = '{"achievementIsDoneID":"1","username":"Admin","isDone":"1","name":"10 kills"}';
+
+
+			//$test = json_decode($json1, true);
+			
+			//echo $test['name'];
+
+			
+			
+			
+		}
+
+	
+	
+	else{
 		$json = array("status" => 0, "msg" => "Username and or achievement is empty");
 	}
 
